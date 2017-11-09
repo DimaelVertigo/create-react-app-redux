b4094ca9afbd8c8a32fe1bba520215e70e0a6c65
Klimenko Dmitry <dimael.vertigo@gmail.com>
2017-11-09 18:38:39 +0200

    combineReducers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import {connect} from 'react-redux';
 
 class App extends Component {
   render() {
+  const { name, surname, age } = this.props.user;
+  const { year } = this.props.page
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to_ {this.props.user}!</h1>
+          <h1 className="App-title">Welcome to_ {name} {surname} {age}!</h1>
+          <p>Year {year}</p>
         </header>
       </div>
     );
@@ -19,7 +21,8 @@ class App extends Component {
 
 function mapStateToProps(state) {
   return {
-    user: state.user
+    user: state.user,
+    page: state.page
   }
 }
 
---
+++ src/reducers/page.js
@@ -0,0 +1,7 @@
+const initialState = {
+  year: '2017'
+};
+
+export default function userstate(state = initialState) {
+  return state;
+};
