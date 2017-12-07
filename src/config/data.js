15d00d10369d3d313e3dcb6136ca3741229472bf
Dmitry Klimenko <dmitryklimenko@Dmitrys-MacBook-Pro.local>
2017-12-07 12:18:04 +0200

    settings

--- src/config/data.js
+++ src/config/data.js
@@ -1,4 +1,4 @@
-export const url = 'https://newdev-hg.das.comodo.od.ua/api/scan-manager/';
+export const url = 'https://newdev-static-hg.das.comodo.od.ua/api/scan-manager/';
 export const apiLogin = 'login';
 export const apiGetList = 'nessus/get-list';
 export const path = '/';
